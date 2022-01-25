83a3398bbbf5727c1241c43b755b8afc2f4b8a51
Ana S. Benedict <anabenedict12@gmail.com>
2022-01-25 11:57:15 -0700

    Updates

---
+++ precache-manifest.907093e7f33e448c0d9547251ff76dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d35ef6b05fccc95986a21db106c4249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5317f02af6f15bd80e1",
+    "url": "/static/css/main.5c50cfa4.chunk.css"
+  },
+  {
+    "revision": "bd7552cc2a8f83fd6a41",
+    "url": "/static/js/2.1dc46864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1dc46864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5317f02af6f15bd80e1",
+    "url": "/static/js/main.6fb68658.chunk.js"
+  },
+  {
+    "revision": "6ba646fe9183d7b0bb98",
+    "url": "/static/js/runtime-main.0008e46a.js"
+  },
+  {
+    "revision": "d89b3d3be7ec3f1296c8d42e253b4a9a",
+    "url": "/static/media/aboutPicture.d89b3d3b.png"
+  },
+  {
+    "revision": "4b64a551571f26ce90d4c2491172b462",
+    "url": "/static/media/asbLogo.4b64a551.svg"
+  }
+]);
